5f724e442903920b1780425e8a58fb7efe3177b1
edsonlcandido <38674440+edsonlcandido@users.noreply.github.com>
2026-03-12 23:38:39 +0000

    Atualiza a URL do PocketBase para o ambiente de desenvolvimento e remove importação não utilizada de parseLocalDate

--- apps/web/src/components/TransactionDrawer.vue
+++ apps/web/src/components/TransactionDrawer.vue
@@ -76,7 +76,6 @@ import { computed, ref, watch } from 'vue'
 import type { RecordModel } from 'pocketbase'
 import type { TransactionPayload } from '../stores/transactions'
 import MonthPicker from './MonthPicker.vue'
-import { parseLocalDate } from '../utils/date'
 
 const props = defineProps<{
   open: boolean
